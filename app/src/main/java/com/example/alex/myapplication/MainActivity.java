9ec5a27f338e2aed42aa579b48bbb7ea6b275389
Alex Kinley <awkinley@gmail.com>
2015-02-19 19:14:14 -0800

    first commiit

---
+++ app/src/main/java/com/example/alex/myapplication/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.alex.myapplication;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.provider.DocumentsContract;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.view.View;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    boolean textChanged = false;
+    boolean hasDoneTests = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setText(View view){
+        DocumentParser documentParser = new DocumentParser();
+
+        AssetManager assetManager = getBaseContext().getAssets();
+        InputStream inputStream = null;
+        try {
+            inputStream = assetManager.open("sampleCSV.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String csvFileString = DocumentParser.copyInputStreamToString(inputStream);
+
+        documentParser.consumeDocument(csvFileString);
+
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.setText("consumed document");
+
+    }
+
+    public void runTests(){
+        Tests test = new Tests();
+        test.testDocumentParserGetLengthReturnsCorrect();
+        test.testDocumentParserGetValueReturnsCorrect();
+    }
+
+    public void runTests(View view){
+        Tests test = new Tests();
+        test.testDocumentParserGetLengthReturnsCorrect();
+        test.testDocumentParserGetValueReturnsCorrect();
+    }
+
+    public android.content.Context getMainActivityBaseContext(){
+        return getBaseContext();
+    }
+}
---
+++ app/src/main/java/com/example/alex/myapplication/Tests.java
@@ -0,0 +1,56 @@
+package com.example.alex.myapplication;
+
+import android.util.Log;
+
+import junit.framework.Assert;
+
+/**
+ * Created by Alex on 2/19/2015.
+ */
+public class Tests {
+
+
+    String sampleCSVFile =
+            "Name, Data 1, Data 2\n" +
+            "column 1, data 1.1, data 1.2\n" +
+            "column 2, data 2.1, data 2.2";
+
+    public void testDocumentParserGetLengthReturnsCorrect(){
+        DocumentParser documentParser = new DocumentParser();
+        documentParser.consumeDocument(sampleCSVFile);
+
+        int docLength = documentParser.getDocLength();
+        boolean areEqual = docLength == 3 ;
+        if(areEqual){
+            Log.v("Tests", "The docLength is correct");
+        }
+        else if(!areEqual){
+            Log.e("Tests", "The docLength is not correct");
+        }
+
+    }
+    public final void testDocumentParserGetValueReturnsCorrect(){
+        DocumentParser documentParser = new DocumentParser();
+        documentParser.consumeDocument(sampleCSVFile);
+
+        String docValue = documentParser.getValue(2,"Name");
+        boolean areEqual = docValue.equals("column 1");
+        if(areEqual){
+            Log.v("Tests", "The docValue is correct");
+        }
+        else if(!areEqual){
+            Log.e("Tests", "The docValue is not correct." + "What was returned was (" + docValue + ")");
+        }
+
+        docValue = null;
+        areEqual = false;
+        docValue = documentParser.getValue(3,"Data 2");
+        areEqual = docValue.equals("data 2.2");
+        if(areEqual){
+            Log.v("Tests", "The docValue is correct");
+        }
+        else if(!areEqual){
+            Log.e("Tests", "The docValue is not correct." + "What was returned was (" + docValue + ")");
+        }
+    }
+}
